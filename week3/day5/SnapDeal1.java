95ac6d64e6afaf7aba9f9edd4ad3a3dcffff4b40
Kavinraj S <68046164+kavinraj2010@users.noreply.github.com>
2022-03-02 20:01:36 +0530

    Update SnapDeal1.java

--- week3/day5/SnapDeal1.java
+++ week3/day5/SnapDeal1.java
@@ -1,9 +1,9 @@
 package week3.day5;
 
-import java.awt.List;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -16,91 +16,98 @@ import org.openqa.selenium.interactions.Actions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SnapDeal1 {
+
 	public static void main(String[] args) throws InterruptedException, IOException {
 		WebDriverManager.chromedriver().setup();
-		ChromeOptions options=new ChromeOptions();
-		ChromeDriver driver=new ChromeDriver(options);
-		options.addArguments("--disable-notifications");
+		
+		//Disable Notifications
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--disable-notifications");
+		ChromeDriver driver = new ChromeDriver(option);
+		
+		//Launch The Url
 		driver.get("https://www.snapdeal.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		
-		Actions build=new Actions(driver);
-		WebElement mensFashion = driver.findElement(By.xpath("(//ul[@class='nav smallNav']//following::span)[2]"));
-		build.moveToElement(mensFashion).build().perform();
+		//Go to Mens Fashion
+		WebElement mensFashion = driver.findElement(By.xpath("//span[@class='catText']"));
+		Actions builder = new Actions(driver);
+		builder.moveToElement(mensFashion).perform();
 		
+		//Go to Sports Shoes & Verify The Title
 		driver.findElement(By.xpath("//span[text()='Sports Shoes']")).click();
-		String shoesCount = driver.findElement(By.xpath("//div[text()='Sports Shoes for Men']/following-sibling::div")).getText();
-		System.out.println("Sports shoe for men Count is : "+shoesCount);
+		String title = driver.getTitle();
+		System.out.println(title);
+		String title2 ="Sports Shoes For Men - Upto 70% OFF on Top Shoe Brands";
+		if(title.equals(title2)) {
+			System.out.println("Title Is Correct");
+		}else {
+			System.out.println("Title Is Incorrect");
+		}
 		
+		//Get the count of the sports shoes
+		Thread.sleep(1500);
+		WebElement shoesCount = driver.findElement(By.xpath("//div[@class='child-cat-name selected']/following-sibling::div"));
+		String text = shoesCount.getText();
+		System.out.println("ShoesCount="+text);
+		
+		//Click Training shoes & Sort by Low to High
 		driver.findElement(By.xpath("//div[text()='Training Shoes']")).click();
-		driver.findElement(By.xpath("//div[@class='sort-selected']")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//ul[@class='sort-value']/li[2]")).click();
+		driver.findElement(By.xpath("//div[@class='sort-drop clearfix']")).click();
+		driver.findElement(By.xpath("(//ul[@class='sort-value']//li)[2]")).click();
 		Thread.sleep(2000);
 		
-		java.util.List<WebElement> product = driver.findElements(By.xpath("//div[@id='products']//span[@class='lfloat product-price']"));
+		//Check if the items displayed are sorted correctly
+		List<WebElement> product = driver.findElements(By.xpath("//div[@id='products']//span[@class='lfloat product-price']"));
 		int size = product.size();
 		System.out.println(size);
+		for(int i =1;i<=size;i++) {
+			WebElement sortPrice = driver.findElement(By.xpath("(//span[@class='lfloat product-price'])["+i+"]"));
+			String text2 = sortPrice.getText();
+			System.out.println("SortedPrice :"+text2);
+		}
 		
-			WebElement sortPrice = driver.findElement(By.xpath("(//span[@class='lfloat product-price'])[1]"));
-			String prize1 = sortPrice.getText();
-			//Thread.sleep(2000);
-			String replaceAll = prize1.replaceAll("\\D", "");
-			//Thread.sleep(2000);
-			//System.out.println(replaceAll);
-			int product1 = Integer.parseInt(replaceAll);
-			
-			for (int i = 2; i < size; i++)
-			{
-				WebElement sortPrice1 = driver.findElement(By.xpath("(//span[@class='lfloat product-price'])["+i+"]"));
-				String prize2 = sortPrice1.getText();
-				String replaceAll1 = prize2.replaceAll("\\D", "");
-				
-				int product2 = Integer.parseInt(replaceAll1);
-				//Thread.sleep(2000);
-				
-				if(product2  >= product1)
-				{
-					
-				}
-				else
-				{
-					System.err.println("Testcase Fail : Not in the selected order");
-				}
-				
-			}
-			System.out.println("The shoes are displayed in a selected order.");
-		
-		driver.findElement(By.xpath("//div[@class='price-input']//input[@name='fromVal']")).clear();
-		driver.findElement(By.xpath("//div[@class='price-input']//input[@name='fromVal']")).sendKeys("900");
-		driver.findElement(By.xpath("//div[@class='price-input']//input[@name='toVal']")).clear();
-		driver.findElement(By.xpath("//div[@class='price-input']//input[@name='toVal']")).sendKeys("1200");
+		//Select the price range (900-1200)
+		WebElement from = driver.findElement(By.xpath("(//div[@class='price-text-box']//input)[1]"));
+		from.clear();
+		from.sendKeys("900");
+		WebElement to = driver.findElement(By.xpath("(//div[@class='price-text-box']//input)[2]"));
+		to.clear();
+		to.sendKeys("1200");
 		driver.findElement(By.xpath("//div[@class='price-go-arrow btn btn-line btn-theme-secondary']")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("(//div[@class='sdCheckbox filters-list '] [5])[1]")).click();
 		
-		WebElement element6 = driver.findElement(By.xpath("(//div[@class='product-tuple-image '])[1]"));
-		Actions click2= new Actions(driver);
-		click2.moveToElement(element6).perform();
-		Thread.sleep(2000);
+		//Filter with color Navy & verify the all applied filters
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//label[@for='Color_s-Navy']//..")).click();
+		WebElement verfyFilter = driver.findElement(By.xpath("(//div[@class='filters'])[1]"));
+		String filter = verfyFilter.getText();
+		System.out.println("Filters Applied : "+filter);
 		
-		WebElement element7 = driver.findElement(By.partialLinkText("Red Tape Walking  Navy Training"));
-		Actions click3=new Actions(driver);
-		click3.moveToElement(element7).perform();
-		
-		driver.findElement(By.xpath("(//div[@class='center quick-view-bar  btn btn-theme-secondary  '])[1]")).click();
-		String text5 = driver.findElement(By.xpath("//span[@class='percent-desc ']")).getText();
-		System.out.println("The discount percemtage is :"+text5);
-		
-		//snapchat 
+		//Mouse Hover on first resulting Training shoes
 		Thread.sleep(2000);
-		File snap=driver.getScreenshotAs(OutputType.FILE);
-		File des=new File("./Images/shoeimage.png");
-		FileUtils.copyFile(snap,des);
-		Thread.sleep(2000);
+		WebElement mouseOver = driver.findElement(By.xpath("(//div[@class='clearfix row-disc'])[1]"));
+		Actions builder1 = new Actions(driver);
+		builder1.moveToElement(mouseOver, 5, 5).build().perform();
+		
+		//click QuickView button
+		driver.findElement(By.xpath("//div[contains(text(),'Quick View')]")).click();
+		
+		//Print the cost and the discount percentage
+		WebElement costDis = driver.findElement(By.xpath("//span[@class='payBlkBig']//.."));
+		String costDiscount = costDis.getText();
+		System.out.println("CostDiscount : "+costDiscount);
+		
+		//Take the snapshot of the shoes.
+		driver.findElement(By.xpath("//div[@class='sidebaroverlay']"));
+		File snapOfShoe = driver.getScreenshotAs(OutputType.FILE);
+		File destination = new File("/.SnapDeal.png");
+		FileUtils.copyFile(snapOfShoe, destination);
+		
+		// Close the current window & Close the main window
+		driver.findElement(By.xpath("((//i[@class='sd-icon sd-icon-delete-sign'])[3])//..")).click();
+		Thread.sleep(500);
 		driver.close();
-		
-		
+		System.out.println("Sucess");
 	}
 }
