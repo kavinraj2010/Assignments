3ff399d574aa25f7f0a8578188691139c2611b55
Kavinraj S <68046164+kavinraj2010@users.noreply.github.com>
2022-03-02 20:00:27 +0530

    Update Nykaa1.java

--- week3/day5/Nykaa1.java
+++ week3/day5/Nykaa1.java
@@ -1,5 +1,4 @@
 package week3.day5;
-
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Set;
@@ -7,98 +6,98 @@ import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Nykaa1 {
+
 	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
-		ChromeOptions options=new ChromeOptions();
-		ChromeDriver driver=new ChromeDriver(options);
-		options.addArguments("--disable-notifications");
+		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.nykaa.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		
-		Actions build=new Actions(driver);
-		WebElement brand = driver.findElement(By.xpath("//ul[@class='HeaderNav css-f7ogli']/li/a[text()='brands']"));
-		build.moveToElement(brand).perform();
+		WebElement brand = driver.findElement(By.xpath("//a[text()='brands']"));
+		Actions builder = new Actions(driver);
+		builder.moveToElement(brand).perform();
 		
-		driver.findElement(By.xpath("//div[@class='brandSearchMain']/input[@id='brandSearchBox']")).sendKeys("L'Oreal Paris");
-		driver.findElement(By.xpath("//div[@id='scroller-container']//a[1]")).click();
+		WebElement search = driver.findElement(By.xpath("(//input[@id='brandSearchBox'])"));
+		search.sendKeys("L'Oreal Paris");
 		
+		driver.findElement(By.linkText("L'Oreal Paris")).click();
 		String title = driver.getTitle();
-		if(title.contains("L'Oreal Paris"))
-		{
-			System.out.println("Testcase Pass, The Title is: "+title);
+		System.out.println(title);
+		String title1 ="Buy L'Oreal Paris products online at best price on Nykaa | Nykaa";
+		
+		if(title1.equals(title)) {
+			System.out.println("The Page You Gone Is Correct");
+		}else {
+			System.out.println("The Page You Gone Is Wrong");
 		}
-		else
-		{
-			System.err.println("TestCase Fail");
-		}
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("(//div[@id='filters-listing']//div)[3]")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//span[text()='customer top rated']/following::div")).click();
-		driver.findElement(By.xpath("//div[@class='sidebar__inner']//span[text()='Category']")).click();
-		driver.findElement(By.xpath("//span[text()='Category']/following::span[text()='Hair']")).click();
-		driver.findElement(By.xpath("//span[text()='Category']/following::span[text()='Hair Care']")).click();
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//span[text()='Category']/following::span[text()='Shampoo']")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//span[text()='Concern']")).click();
-		driver.findElement(By.xpath("//span[text()='Color Protection']")).click();
-		String filter = driver.findElement(By.xpath("//span[@class='filter-value'][1]")).getText();
-		if(filter.contains("Shampoo"))
-		{
-			System.out.println("The Filter Shampoo is selected");
-		}
-		else
-		{
-			System.err.println("Shampoo is not selected");
-		}
-		driver.findElement(By.xpath("//div[@id='product-list-wrap']//following::img[1]")).click();
-		//driver.findElement(By.xpath("(//div[@class='css-1rd7vky'])[1]")).click();
+		//Click sort By and select customer top rated
+		driver.findElement(By.xpath("//div[@id='filter-sort']//button")).click();
+		driver.findElement(By.xpath("(//div[@class='control-indicator radio '])[3]")).click();
+		
+		//Click Category and click Hair->Click haircare->Shampoo
+		driver.findElement(By.xpath("//div[@id='first-filter']/div")).click();
+		driver.findElement(By.xpath("//span[text()='Hair']/..")).click();
+		driver.findElement(By.xpath("//span[text()='Hair Care']/..")).click();
+		driver.findElement(By.xpath("//input[@id='checkbox_Shampoo_316']/..")).click();
+		
+		// Click->Concern->Color Protection
+		driver.findElement(By.xpath("//span[text()='Concern']/..")).click();
+		driver.findElement(By.xpath("//input[@id='checkbox_Color Protection_10764']//..")).click();
+		
+		//check whether the Filter is applied with Shampoo
+		WebElement filterCheck = driver.findElement(By.xpath("(//div[@id='filters-listing']//div)[3]"));
+		String filter = filterCheck.getText();
+		System.out.println("FiltersApplied : "+filter);
+		
+		//Click on L'Oreal Paris Colour Protect Shampoo & GO to the new window and select size as 175ml
+		driver.findElement(By.xpath("(//div[@class='css-1rd7vky'])[1]")).click();
 		Thread.sleep(500);
 		
+		//Switch To Next Tab (To Access another Window)
 		Set<String> windowHandles = driver.getWindowHandles();
 		System.out.println(windowHandles);
 		ArrayList<String> value =new ArrayList<String>(windowHandles);
 		driver.switchTo().window(value.get(1));
 		String currentUrl = driver.getCurrentUrl();
-		System.out.println(currentUrl);		
+		System.out.println(currentUrl);
 		
-		WebElement size = driver.findElement(By.xpath("//select[@title='SIZE']"));
-		Select dropDown1=new Select(size);
-		dropDown1.selectByVisibleText("175ml");
+	    //select size as 175ml
+		WebElement dropdown = driver.findElement(By.xpath("(//select[@title='SIZE'])"));
+		Select drop =new Select(dropdown);
+		drop.selectByVisibleText("175ml");
 		
-		String mrp = driver.findElement(By.xpath("//span[text()='MRP:']/span")).getText();
-		System.out.println("MRP of the product: "+mrp);
+		//Print MRP and Click On Add To Bags
+		WebElement getMrp = driver.findElement(By.xpath("(//div[@class='css-k400rm']//span)[3]"));
+		String Mrp = getMrp.getText();
+		System.out.println("MRP :"+Mrp);
 		
-		driver.findElement(By.xpath("//button/span[text()='ADD TO BAG']")).click();
+		//Click on ADD to BAG &  Go to Shopping Bag &  Print the Grand Total amount 
+		WebElement addToBags = driver.findElement(By.xpath("//span[text()='ADD TO BAG']/.."));
+		addToBags.click();
+		driver.findElement(By.xpath("//span[@class='cart-count']//..")).click();
 		Thread.sleep(2000);
 		
-		driver.findElement(By.xpath("//span[text()='Account']//following::button")).click();
+		//Click Proceed
 		driver.switchTo().frame(0);
-		String grandTotal = driver.findElement(By.xpath("(//div[@class='first-col'])/div[@class='value']")).getText();
-		System.out.println("The Grand Total Amount is : "+grandTotal);
+		String grandTotal = driver.findElement(By.xpath("(//div[@class='first-col'])")).getText();
+		System.out.println(grandTotal);
+		driver.findElement(By.xpath("//span[text()='PROCEED']//..//..")).click();
 		
-		driver.findElement(By.xpath("//span[text()='PROCEED']")).click();
-		Thread.sleep(2000);
+		// Click Continue As Guest	& Check The grand Total
 		driver.findElement(By.xpath("//button[text()='CONTINUE AS GUEST']")).click();
-		Thread.sleep(2000);
-		String text = driver.findElement(By.xpath("//div[text()='Grand Total']//following::div")).getText();
-		if(grandTotal.contains(text))
-		{
-			System.out.println("TestCase Pass");
-			driver.quit();
+		if(Mrp.equals(grandTotal)) {
+			System.out.println("The Grand Total Is Same");
+		}else {
+			System.out.println("The Grand Total Is Not Same");
 		}
-		else
-		{
-			System.err.println("TestCase Fail");
-		}
+		driver.quit();
+		//driver.close();
 	}
 }
